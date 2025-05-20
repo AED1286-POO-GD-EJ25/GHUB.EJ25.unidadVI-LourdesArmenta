73771af32cb8279f59b2b7ef9a5656d6a171b47f
armenta lindoro maria lourdes <137097961+mariaLourdesArmenta@users.noreply.github.com>
2025-05-20 16:00:20 +0000

    operaciones con ficheros

---
+++ miPrincipal/LeerArchivoBinario.java
@@ -0,0 +1,18 @@
+package miPrincipal;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+
+public class LeerArchivoBinario {
+    public static void main(String[] args) {
+        try(FileInputStream fis = new FileInputStream("datos.dat")){
+            int byteLeido;
+            while ((byteLeido = fis.read()) != -1){
+                System.out.print(byteLeido+" ");
+            }
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+    }
+    
+}
